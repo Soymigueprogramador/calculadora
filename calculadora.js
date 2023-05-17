747d0a954d34dab5f22a07fec2bcce6c24cd082d
Miguel Salazar <yobebe@hotmail.com.ar>
2023-05-16 21:12:25 -0300

    estoy cargando el js de la calculadora

---
+++ calculadora.js
@@ -0,0 +1,34 @@
+//antes de que me olvide voy a conectar todos los id del dom. 
+const displayValue = document.getElementById("displayValue");
+const valorAnterior = document.getElementById("valorAnterior");
+const valorActual = document.getElementById("valorActual");
+const c = document.getElementById("c");
+const signos = document.getElementById("signos");
+const botonNumero = document.getElementById("botonNumero");
+const punto = document.getElementById("punto");
+//voy a crear unas variables para las operaciones. 
+let sumar = "";
+let resta = "";
+let multiplicar = "";
+let dividir = "";
+let operaciones = "";
+let resultado = "";
+//voy a crear una clase para meter todo.
+class calculadora {
+  sumar(numero1, numero2) {
+    return numero1 + numero2;
+  }
+  restar(numero1, numero2) {
+    return numero1 - numero2;
+  }
+  multiplicar(numero1, numero2) {
+    return numero1, numero2
+  }
+  dividir(numero1, numero2) {
+    return numero1, numero2;
+  }
+}
+//voy a crear las funciones de la calculadora. 
+function calculadora() {
+  
+} 
