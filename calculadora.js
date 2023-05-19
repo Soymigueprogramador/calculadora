553d09de5dc48d2b65eef5775c663be067edb85c
Miguel Salazar <yobebe@hotmail.com.ar>
2023-05-19 20:35:00 -0300

    agrege cambios al diseño y al js

--- calculadora.js
+++ calculadora.js
@@ -1,34 +1,44 @@
-//antes de que me olvide voy a conectar todos los id del dom. 
-const displayValue = document.getElementById("displayValue");
-const valorAnterior = document.getElementById("valorAnterior");
-const valorActual = document.getElementById("valorActual");
-const c = document.getElementById("c");
-const signos = document.getElementById("signos");
-const botonNumero = document.getElementById("botonNumero");
-const punto = document.getElementById("punto");
-//voy a crear unas variables para las operaciones. 
-let sumar = "";
-let resta = "";
-let multiplicar = "";
-let dividir = "";
-let operaciones = "";
-let resultado = "";
-//voy a crear una clase para meter todo.
-class calculadora {
-  sumar(numero1, numero2) {
-    return numero1 + numero2;
-  }
-  restar(numero1, numero2) {
-    return numero1 - numero2;
-  }
-  multiplicar(numero1, numero2) {
-    return numero1, numero2
-  }
-  dividir(numero1, numero2) {
-    return numero1, numero2;
-  }
-}
-//voy a crear las funciones de la calculadora. 
-function calculadora() {
-  
-} 
+//aca conecte la pantalla y los botones.
+const pantalla = document.querySelector(".pantalla");
+const botones = document.querySelectorAll(".btn");
+//aca intento que se guarden los resultados en el local storage. 
+let resultados = JSON.parse(localStorage.getItem("resultados")) || [];
+//aca arrancamos con los eventos de los botones y con la calculadora. 
+botones.forEach(boton => {
+    boton.addEventListener("click", () => {
+        const botonApretado = boton.textContent;
+        //un condicional en la que si el sistema detecta "c" borra todo y muestra un "0". 
+        if (boton.id === "c") {
+            pantalla.textContent = "0";
+            return;
+        }
+        //si el sistema detecta una operacion erronea va a mostrar un "invalido",
+        //"ejemplo: 2 / 2 = invalido". 
+        if (boton.id === "borrar") {
+            if (pantalla.textContent.length === 1 || pantalla.textContent === "Error!") {
+                pantalla.textContent = "0";
+            } else {
+                pantalla.textContent = pantalla.textContent.slice(0, -1);
+            }
+            return;
+        }
+        //despues de cada operacion apretamos el boton de igual y nos da el resultado. 
+        if (boton.id === "igual") {
+            try {
+                const resultado = eval(pantalla.textContent);
+                pantalla.textContent = resultado;
+                resultados.push(resultado);
+                localStorage.setItem("resultados", JSON.stringify(resultados));
+            } catch {
+                pantalla.textContent = "Error!";
+            }
+            return;
+        }
+        
+        if (pantalla.textContent === "0" || pantalla.textContent === "Error!") {
+            pantalla.textContent = botonApretado;
+        } else {
+            pantalla.textContent += botonApretado;
+        }
+    })
+});
